1b089a31436fc4a46cb4cfa2a3aaa19933e8819b
Tabutsa <davit.tabutsadze.1@btu.edu.ge>
2019-01-04 17:10:46 +0400

    cathTheNikaTheGame

---
+++ app/src/main/java/com/example/tabutsa/catchthenikagame/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.tabutsa.catchthenikagame
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.os.Handler
+import android.view.View
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    var score : Int = 0
+    var imageArray = ArrayList<ImageView>()
+    var handler : Handler = Handler()
+    var runnable : Runnable = Runnable {  }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        score = 0
+        imageArray = arrayListOf(imageView, imageView1, imageView2, imageView3, imageView4, imageView5, imageView6, imageView7, imageView8)
+
+        hideImages()
+
+        object = CountDownTimer(10000, 1000) {
+            override fun onFinish() {
+                timeText.text = "Time's Up!"
+                handler.removeCallbacks(runnable)
+                for (image in imageArray) {
+                    image.visibility = View.INVISIBLE
+                }
+            override fun onTick(p0: Long) {
+                timeText.text = "Time: " + p0 / 1000
+        }
+        }.start()
+    }
+
+    fun hideImages() {
+        runnable = object : Runnable {
+            override fun run() {
+                for (image in imageArray) {
+                    image.visibility = View.INVISIBLE
+                }
+                val random = Random()
+                val index  = random.nextInt(8 - 0)
+                imageArray[index].visibility = View.VISIBLE
+
+                handler.postDelayed(runnable, 200)
+            }
+        }
+        handler.post(runnable)
+
+    }
+
+
+    }
+
+    fun increaseScore(view: View) {
+        score++
+
+        scoreText.text = "Score :" + score
+    }
+
+}
